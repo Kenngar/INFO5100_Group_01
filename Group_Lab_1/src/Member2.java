9779767eecf93d70a96dd95f86286ed627c4afdb
126anguyen <nguyen.a12@northeastern.edu>
2026-06-17 21:41:59 -0700

    Add greeting from Member 2

--- Group_Lab_1/src/Member2.java
+++ Group_Lab_1/src/Member2.java
@@ -7,12 +7,8 @@
  *
  * @author Kenneth Garcia
  */
-public class Main {
-    
-    public static void main(String[] args) {     
-        Member1.main(args);  
-        Member2.main(args);
-
+public class Member2 {
+    public static void main(String[]args){
+        System.out.println("Hello from Member 2!");
     }
-    
 }
